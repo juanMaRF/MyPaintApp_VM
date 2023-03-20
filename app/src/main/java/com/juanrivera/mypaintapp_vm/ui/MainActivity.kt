ca292af29b3e689ad547c991729ecc5c6da33ef4
Hp <juanm.rivera@udea.edu.co>
2023-03-20 18:19:21 -0500

    Initial commit

---
+++ app/src/main/java/com/juanrivera/mypaintapp_vm/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.juanrivera.mypaintapp_vm.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.juanrivera.mypaintapp_vm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
